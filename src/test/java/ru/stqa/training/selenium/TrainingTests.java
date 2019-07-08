ce9b4a6c4b0d05a9e0b29546eb88f4c0c4ac5ba7
Vitaly Losikov <losikov@list.ru>
2019-07-08 18:43:30 +0300

    TestBase

--- src/test/java/ru/stqa/training/selenium/TrainingTests.java
+++ src/test/java/ru/stqa/training/selenium/TrainingTests.java
@@ -1,30 +1,12 @@
 package ru.stqa.training.selenium;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    @Before
-    public void start()
-    {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
-    }
+public class TrainingTests extends TestBase {
 
     @Test
     public void myFirstTest()
@@ -43,12 +25,5 @@ public class MyFirstTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-    }
-
-    @After
-    public void stop()
-    {
-        driver.quit();
-        driver = null;
     }
 }
