a132ab5d571ee7f742536afebdbfd5f62c26c47a
Vitaly Losikov <losikov@list.ru>
2019-07-11 12:00:22 +0300

    task8_v2

--- src/test/java/ru/stqa/training/selenium/Task8.java
+++ src/test/java/ru/stqa/training/selenium/Task8.java
@@ -3,7 +3,7 @@ package ru.stqa.training.selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class Task8 extends TestBase {
 
@@ -16,18 +16,12 @@ public class Task8 extends TestBase {
         checkStickersInBox("div#box-latest-products li");
     }
 
-    private boolean isElementPresent(By locator) {
-        return driver.findElements(locator).size() > 0;
-    }
-
     private void checkStickersInBox(String selector) {
         int size = driver.findElements(By.cssSelector(selector)).size();
 
         for (int i = 1; i < size + 1; i++) {
-            assertTrue(isElementPresent(By.cssSelector(selector + ":nth-child(" + i + ") div.sticker:nth-of-type(1)")));
-            assertTrue(!isElementPresent(By.cssSelector(selector + ":nth-child(" + i + ") div.sticker:nth-of-type(2)")));
-            System.out.println(
-                    driver.findElement(By.cssSelector(selector + ":nth-child(" + i + ") div.sticker:nth-of-type(1)")).getText());
+            assertEquals(1, driver.findElements(By.cssSelector(selector + ":nth-child(" + i + ") div.sticker")).size());
+            System.out.println(driver.findElement(By.cssSelector(selector + ":nth-child(" + i + ") div.sticker")).getText());
         }
     }
 }
