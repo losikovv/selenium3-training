6428d18edf365eb1dfa1c55cc37f0bf2eb0815c3
Vitaly Losikov <losikov@list.ru>
2019-07-15 20:50:30 +0300

    geoZonesOrder

--- src/test/java/ru/stqa/training/selenium/Task9.java
+++ src/test/java/ru/stqa/training/selenium/Task9.java
@@ -26,7 +26,7 @@ public class Task9 extends TestBase {
             int zonesNumber = Integer.parseInt(driver.findElements(By.xpath("//td[6]")).get(i).getText());
             if (zonesNumber > 0) {
                 driver.findElements(By.xpath("//td[5]//a")).get(i).click();
-                checkElementsOrder(By.xpath("//td[3]/input[@type='hidden']"));
+                checkElementsOrder(By.xpath("//td[3]/input[@type='hidden']"), "value");
                 driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
             }
         }
@@ -36,6 +36,13 @@ public class Task9 extends TestBase {
     public void geoZonesOrder() {
         authAdmin();
         driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+        int zonesSize = driver.findElements(By.xpath("//td[3]/a")).size();
+
+        for (int i = 0; i < zonesSize; i++) {
+                driver.findElements(By.xpath("//td[5]//a")).get(i).click();
+                checkElementsOrder(By.xpath("//td[3]/select/option[@selected]"));
+                driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+            }
 
     }
 
@@ -50,7 +57,23 @@ public class Task9 extends TestBase {
         List<WebElement> elements = driver.findElements(locator);
 
         for (int i = 0; i < elements.size() - 1; i++) {
-            Assert.assertTrue(elements.get(i).getText().compareToIgnoreCase(elements.get(i + 1).getText()) < 1);
+            Assert.assertTrue(elements.get(i)
+                    .getText().compareToIgnoreCase(elements.get(i + 1).getText()) < 1);
+
+            System.out.print(elements.get(i).getText() + " < ");
+            System.out.println(elements.get(i + 1).getText());
+        }
+    }
+
+    private void checkElementsOrder(By locator, String attribute) {
+        List<WebElement> elements = driver.findElements(locator);
+
+        for (int i = 0; i < elements.size() - 1; i++) {
+            Assert.assertTrue(elements.get(i).getAttribute(attribute)
+                    .compareToIgnoreCase(elements.get(i + 1).getAttribute(attribute)) < 1);
+
+            System.out.print(elements.get(i).getAttribute(attribute) + " < ");
+            System.out.println(elements.get(i + 1).getAttribute(attribute));
         }
     }
 }
