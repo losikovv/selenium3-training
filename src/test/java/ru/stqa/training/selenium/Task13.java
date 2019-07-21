826d7bd1589843e91335efbe96ebf2644f720673
Vitaly Losikov <losikov@list.ru>
2019-07-21 15:26:07 +0300

    edit task13

--- src/test/java/ru/stqa/training/selenium/Task13.java
+++ src/test/java/ru/stqa/training/selenium/Task13.java
@@ -1,6 +1,5 @@
 package ru.stqa.training.selenium;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,29 +8,34 @@ public class Task13 extends TestBase {
 
     @Test
     public void cart() {
+        addToCart(3);
+        clearCart();
+    }
 
-        for (int i = 0; i < 3; i++) {
+    private void clearCart() {
+        driver.findElement(By.xpath("//a[contains(text(),'Checkout »')]")).click();
+
+        By items = By.xpath("//td[@class='item']");
+        int size = driver.findElements(items).size();
+        for (int i = 0; i < size; i++) {
+            size = driver.findElements(items).size();
+            driver.findElement( By.xpath("//button[@value='Remove']")).click();
+            wait.until(ExpectedConditions.numberOfElementsToBe(items, size - 1));
+        }
+    }
+
+    private void addToCart(int number) {
+        for (int i = 0; i < number; i++) {
             driver.get("http://localhost/litecart/en/");
             driver.findElement(By.xpath("//li[@class='product column shadow hover-light']")).click();
 
             By option = By.xpath("//select[@name='options[Size]']");
             if (driver.findElements(option).size() > 0) driver.findElement(option).sendKeys("small");
 
-            By qtyLocator = By.xpath("//span[@class='quantity']");
-            int quantityOfItems = Integer.parseInt(driver.findElement(qtyLocator).getText());
+            By quantity = By.xpath("//span[@class='quantity']");
+            int before = Integer.parseInt(driver.findElement(quantity).getText());
             driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
-            wait.until(ExpectedConditions.textToBe(qtyLocator, Integer.toString(quantityOfItems + 1)));
+            wait.until(ExpectedConditions.textToBe(quantity, Integer.toString(before + 1)));
         }
-
-        driver.findElement(By.xpath("//a[contains(text(),'Checkout »')]")).click();
-
-        By remove = By.xpath("//button[@value='Remove']");
-        int size = driver.findElements(remove).size();
-        for (int i = 0; i < size; i++) {
-        driver.findElement(remove).click();
-        wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.xpath("//div[@id='order_confirmation-wrapper']"))));
-        }
-        Assert.assertTrue(driver.findElements(
-                By.xpath("//em[contains(text(),'There are no items in your cart.')]")).size() > 0);
     }
 }
